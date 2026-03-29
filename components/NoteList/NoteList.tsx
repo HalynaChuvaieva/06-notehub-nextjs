d6478adc1e76153d774e05b917567706743a5eae
Halyna <galacuvaeva4@gmail.com>
2026-03-29 16:03:41 +0300

    add path by id

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -21,7 +21,7 @@ export default function NoteList({ notes }: NoteListProps) {
           <p className={css.content}>{content}</p>
           <div className={css.footer}>
             <span className={css.tag}>{tag}</span>
-            <Link href="``">View details</Link>
+            <Link href={`/notes/${id}`}>View details</Link>
             <button onClick={() => mutation.mutate(id)} className={css.button}>
               Delete
             </button>
